6bcfce4e4a845efa36985688bc1640c9c5cdfbb8
alpha knyfe <rajeevkl07@gmail.com>
2018-10-22 02:49:55 +0530

    Initial commit

---
+++ app/src/main/java/com/alphaknyfe/prodot6000/securityservice/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alphaknyfe.prodot6000.securityservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
